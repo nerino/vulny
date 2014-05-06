dba547961234bd90770cbe3d49b2f27a9948a007
Nera <nera.nesic@gmail.com>
2014-05-06 17:03:45 +0000

    things

---
+++ vulny/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vulny::Application.config.secret_token = '106ac54115ac97d79c61c847e0d101ba71a6e2f47e5fbf2c30b89475a73a8cf29c474a1585fcd619cb9136908bd5fe8286fa41893dac90dcd2185335b6885b73'
